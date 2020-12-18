83f6a579ecf91d3e03096da793ee34e0db97ef50
Valiantsin Ulasau <valiantsin.ulasau@dkfz-heidelberg.de>
2020-12-18 16:08:12 +0100

    Changing default password.

--- Unite.Data.Migrations/Configuration/EnvironmentConfig.cs
+++ Unite.Data.Migrations/Configuration/EnvironmentConfig.cs
@@ -7,7 +7,7 @@ namespace Unite.Data.Migrations.Configuration
         private static string _defaultMySqlHost = "localhost";
         private static string _defaultMySqlDatabase = "unite";
         private static string _defaultMySqlUser = "root";
-        private static string _defaultMySqlPassword = "12345";
+        private static string _defaultMySqlPassword = "Long-p@55w0rd";
 
         public static string MySqlHost => GetEnvironmentVariable("UNITE_MYSQL_HOST", _defaultMySqlHost);
         public static string MySqlDatabase = GetEnvironmentVariable("UNITE_MYSQL_DATABASE", _defaultMySqlDatabase);
