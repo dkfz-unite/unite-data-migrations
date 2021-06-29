f11068f1543633261d8a1efdd2a8e3702c44d492
Valiantsin Ulasau <valiantsin.ulasau@gmail.com>
2021-06-29 16:37:53 +0200

    Sample reference id.

--- Unite.Data.Migrations/Migrations/20210629143603_sample-reference.Designer.cs
+++ Unite.Data.Migrations/Migrations/20210629143603_sample-reference.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Unite.Data.Services;
@@ -9,9 +10,10 @@ using Unite.Data.Services;
 namespace Unite.Data.Migrations.Migrations
 {
     [DbContext(typeof(UniteDbContext))]
-    partial class UniteDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210629143603_sample-reference")]
+    partial class samplereference
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Unite.Data.Migrations/Migrations/20210629143603_sample-reference.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Unite.Data.Migrations.Migrations
+{
+    public partial class samplereference : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ReferenceId",
+                table: "Samples",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReferenceId",
+                table: "Samples");
+        }
+    }
+}
