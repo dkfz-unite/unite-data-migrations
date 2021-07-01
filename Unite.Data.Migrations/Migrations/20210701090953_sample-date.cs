55547233e461e277a177fa5cbb5ab450ad9fb2cd
Valiantsin Ulasau <valiantsin.ulasau@gmail.com>
2021-07-01 14:35:53 +0200

    Moved molecular data from general specimen to specific type specimens.

---
+++ Unite.Data.Migrations/Migrations/20210701090953_sample-date.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Unite.Data.Migrations.Migrations
+{
+    public partial class sampledate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "Samples");
+
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "Analyses");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Date",
+                table: "Samples",
+                type: "timestamp without time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Date",
+                table: "Analyses",
+                type: "timestamp without time zone",
+                nullable: true);
+        }
+    }
+}
