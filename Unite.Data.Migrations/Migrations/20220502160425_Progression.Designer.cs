ba2044c7c7d124303e1be553b654ee4183222188
vulasau <valiantsin.ulasau@gmail.com>
2022-05-02 18:09:51 +0200

    Moved migration status data from treatment to clinical data.

--- Unite.Data.Migrations/Migrations/20220502160425_Progression.Designer.cs
+++ Unite.Data.Migrations/Migrations/20220502160425_Progression.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Unite.Data.Services;
@@ -9,9 +10,10 @@ using Unite.Data.Services;
 namespace Unite.Data.Migrations.Migrations
 {
     [DbContext(typeof(DomainDbContext))]
-    partial class DomainDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220502160425_Progression")]
+    partial class Progression
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
