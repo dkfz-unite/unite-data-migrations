84a7cef55c011abb071faa6f3044973f50bb7e9e
vulasau <valiantsin.ulasau@gmail.com>
2022-06-29 12:15:17 +0200

    Work package description.

---
+++ Unite.Data.Migrations/Migrations/20220629101137_WorkPackageDescription.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Unite.Data.Migrations.Migrations
+{
+    public partial class WorkPackageDescription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                schema: "don",
+                table: "WorkPackages",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                schema: "don",
+                table: "WorkPackages");
+        }
+    }
+}
