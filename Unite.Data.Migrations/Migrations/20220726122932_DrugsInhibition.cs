6af866e47a592304a301f624eee88a3e1215eb85
vulasau <valiantsin.ulasau@gmail.com>
2022-07-27 17:53:33 +0200

    Extended drug screening data.

---
+++ Unite.Data.Migrations/Migrations/20220726122932_DrugsInhibition.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Unite.Data.Migrations.Migrations
+{
+    public partial class DrugsInhibition : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Gof",
+                schema: "spe",
+                table: "DrugScreenings",
+                type: "double precision",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Inhibition",
+                schema: "spe",
+                table: "DrugScreenings",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Gof",
+                schema: "spe",
+                table: "DrugScreenings");
+
+            migrationBuilder.DropColumn(
+                name: "Inhibition",
+                schema: "spe",
+                table: "DrugScreenings");
+        }
+    }
+}
