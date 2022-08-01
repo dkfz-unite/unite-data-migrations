2a7e25298bb23b9f19dedadb08f85b3a387e12de
vulasau <valiantsin.ulasau@gmail.com>
2022-08-01 16:45:50 +0200

    Drug response curve data.

---
+++ Unite.Data.Migrations/Migrations/20220801143646_DrugsResponse.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Unite.Data.Migrations.Migrations
+{
+    public partial class DrugsResponse : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Inhibition",
+                schema: "spe",
+                table: "DrugScreenings",
+                newName: "InhibitionsSample");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Concentrations",
+                schema: "spe",
+                table: "DrugScreenings",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Inhibitions",
+                schema: "spe",
+                table: "DrugScreenings",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "InhibitionsControl",
+                schema: "spe",
+                table: "DrugScreenings",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Concentrations",
+                schema: "spe",
+                table: "DrugScreenings");
+
+            migrationBuilder.DropColumn(
+                name: "Inhibitions",
+                schema: "spe",
+                table: "DrugScreenings");
+
+            migrationBuilder.DropColumn(
+                name: "InhibitionsControl",
+                schema: "spe",
+                table: "DrugScreenings");
+
+            migrationBuilder.RenameColumn(
+                name: "InhibitionsSample",
+                schema: "spe",
+                table: "DrugScreenings",
+                newName: "Inhibition");
+        }
+    }
+}
