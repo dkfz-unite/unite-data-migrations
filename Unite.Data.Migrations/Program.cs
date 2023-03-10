2fdd6e8d343b5adf5e98416588a82a83e9ff6c7d
vulasau <valiantsin.ulasau@gmail.com>
2023-03-10 14:28:42 +0100

    Update data package.

--- Unite.Data.Migrations/Program.cs
+++ Unite.Data.Migrations/Program.cs
@@ -2,49 +2,6 @@
 using Unite.Data.Migrations.Configuration.Options;
 using Unite.Data.Services;
 
-//namespace Unite.Data.Migrations;
-
-//class Program
-//{
-//    static void Main(string[] args)
-//    {
-//        Console.WriteLine($"[{DateTime.UtcNow}] - Migration service started");
-
-
-//        Console.WriteLine($"[{DateTime.UtcNow}] - Configuring migration service...");
-
-//        var options = new SqlOptions();
-
-
-//        Console.WriteLine($"[{DateTime.UtcNow}] - Creating database context...");
-
-//        var dbContext = new DomainDbContext(options);
-
-
-//        Console.WriteLine($"[{DateTime.UtcNow}] - Starting migration...");
-
-//        try
-//        {
-//            dbContext.Database.Migrate();
-
-//            Console.WriteLine($"[{DateTime.UtcNow}] - Migration completed successfully");
-//        }
-//        catch (Exception exception)
-//        {
-//            Console.WriteLine($"[{DateTime.UtcNow}] - Migration failed");
-
-//            Console.WriteLine(exception.Message);
-
-//            if (exception.InnerException != null)
-//            {
-//                Console.WriteLine(exception.InnerException.Message);
-//            }
-//        }
-
-//        Console.WriteLine($"[{DateTime.UtcNow}] - Migration service stopped");
-//    }
-//}
-
 
 Console.WriteLine($"[{DateTime.UtcNow}] - Migration service started");
 
