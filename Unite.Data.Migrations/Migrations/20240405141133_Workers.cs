a081b37e068e473d8b7c8b24630dadf13dc6010b
vulasau <valiantsin.ulasau@gmail.com>
2024-04-11 12:52:48 +0200

    Specific tasks workers.

---
+++ Unite.Data.Migrations/Migrations/20240405141133_Workers.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Unite.Data.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class Workers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WorkerTypes",
+                schema: "com",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false),
+                    Value = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkerTypes", x => x.Id);
+                    table.UniqueConstraint("AK_WorkerTypes_Value", x => x.Value);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Workers",
+                schema: "com",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    TypeId = table.Column<int>(type: "integer", nullable: false),
+                    Active = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Workers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Workers_WorkerTypes_TypeId",
+                        column: x => x.TypeId,
+                        principalSchema: "com",
+                        principalTable: "WorkerTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                schema: "com",
+                table: "WorkerTypes",
+                columns: new[] { "Id", "Name", "Value" },
+                values: new object[,]
+                {
+                    { 1, "Submission", "Submission" },
+                    { 2, "Annotation", "Annotation" },
+                    { 3, "Indexing", "Indexing" }
+                });
+
+            migrationBuilder.InsertData(
+                schema: "com",
+                table: "Workers",
+                columns: new[] { "Id", "Active", "TypeId" },
+                values: new object[,]
+                {
+                    { 1, true, 1 },
+                    { 2, true, 2 },
+                    { 3, true, 3 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Workers_TypeId",
+                schema: "com",
+                table: "Workers",
+                column: "TypeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Workers",
+                schema: "com");
+
+            migrationBuilder.DropTable(
+                name: "WorkerTypes",
+                schema: "com");
+        }
+    }
+}
