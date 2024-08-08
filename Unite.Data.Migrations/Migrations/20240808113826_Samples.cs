b882f6a95147128c8133d4ed332fde8093ac6a10
vulasau <valiantsin.ulasau@gmail.com>
2024-08-08 16:14:18 +0200

    New migration script.

---
+++ Unite.Data.Migrations/Migrations/20240808113826_Samples.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Unite.Data.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class Samples : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "GenesModel",
+                schema: "gen",
+                table: "Samples",
+                newName: "Genome");
+
+            migrationBuilder.RenameColumn(
+                name: "CellsNumber",
+                schema: "gen",
+                table: "Samples",
+                newName: "Cells");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Genome",
+                schema: "gen",
+                table: "Samples",
+                newName: "GenesModel");
+
+            migrationBuilder.RenameColumn(
+                name: "Cells",
+                schema: "gen",
+                table: "Samples",
+                newName: "CellsNumber");
+        }
+    }
+}
