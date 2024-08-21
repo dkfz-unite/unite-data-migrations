898a095898fcb66c37ebc4a4f415c557a827dfe5
vulasau <valiantsin.ulasau@gmail.com>
2024-08-21 11:59:37 +0200

    Single cell analysis task migration.

---
+++ Unite.Data.Migrations/Migrations/20240821095712_ScAnalysis.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Unite.Data.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class ScAnalysis : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "com",
+                table: "AnalysisTaskTypes",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.InsertData(
+                schema: "com",
+                table: "AnalysisTaskTypes",
+                columns: new[] { "Id", "Name", "Value" },
+                values: new object[,]
+                {
+                    { 1, "RNA-DE", "RNA-DE" },
+                    { 2, "RNASC", "RNASC" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "com",
+                table: "AnalysisTaskTypes",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                schema: "com",
+                table: "AnalysisTaskTypes",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.InsertData(
+                schema: "com",
+                table: "AnalysisTaskTypes",
+                columns: new[] { "Id", "Name", "Value" },
+                values: new object[] { 1, "DExp", "DExp" });
+        }
+    }
+}
