3c2e1374031f6fb9ccb23931120b803d32a47900
gmlavanya <lavanya.gmoorthy@gmail.com>
2025-02-06 13:27:00 +0100

    Added submission type dna-meth

---
+++ Unite.Data.Migrations/Migrations/20250206120843_Methylation.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Unite.Data.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class Methylation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                schema: "com",
+                table: "SubmissionTaskTypes",
+                columns: new[] { "Id", "Name", "Value" },
+                values: new object[] { 304, "dna-meth", "dna-meth" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "com",
+                table: "SubmissionTaskTypes",
+                keyColumn: "Id",
+                keyValue: 304);
+        }
+    }
+}
