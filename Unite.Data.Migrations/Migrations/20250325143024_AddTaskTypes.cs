541999174d3625c111661e5db60e276fa3af6aef
gmlavanya <lavanya.gmoorthy@gmail.com>
2025-03-25 15:37:36 +0100

    Analysis task type meth added

---
+++ Unite.Data.Migrations/Migrations/20250325143024_AddTaskTypes.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Unite.Data.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTaskTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                schema: "com",
+                table: "AnalysisTaskTypes",
+                columns: new[] { "Id", "Name", "Value" },
+                values: new object[] { 4, "meth", "meth" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "com",
+                table: "AnalysisTaskTypes",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
