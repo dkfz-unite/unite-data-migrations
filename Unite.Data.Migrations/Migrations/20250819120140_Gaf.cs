a8be5145af51a02a079579d9043726aae98e6f30
vulasau <valiantsin.ulasau@gmail.com>
2025-08-19 14:02:34 +0200

    Gene alteration frequency analysis migration.

---
+++ Unite.Data.Migrations/Migrations/20250819120140_Gaf.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Unite.Data.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class Gaf : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                schema: "com",
+                table: "analysis_task_type",
+                columns: new[] { "id", "name", "value" },
+                values: new object[] { 6, "gaf", "gaf" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "com",
+                table: "analysis_task_type",
+                keyColumn: "id",
+                keyValue: 6);
+        }
+    }
+}
