dc4819d00d6e5dcd079ebfcd47250ab1675e8bfc
vulasau <valiantsin.ulasau@gmail.com>
2025-11-07 14:31:04 +0100

    Gene knockouts.

---
+++ Unite.Data.Migrations/Migrations/20251107132934_Knockouts.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Unite.Data.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class Knockouts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "gene_knockouts",
+                schema: "spe",
+                table: "molecular_data",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "gene_knockouts",
+                schema: "spe",
+                table: "molecular_data");
+        }
+    }
+}
