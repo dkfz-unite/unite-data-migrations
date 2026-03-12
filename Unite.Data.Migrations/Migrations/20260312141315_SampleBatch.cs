1c078c656c939c912d67f9997959d8e1672eeb6d
vulasau <valiantsin.ulasau@gmail.com>
2026-03-12 15:22:16 +0100

    New sample batch migration.

---
+++ Unite.Data.Migrations/Migrations/20260312141315_SampleBatch.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Unite.Data.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class SampleBatch : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "batch",
+                schema: "omi",
+                table: "sample",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "batch",
+                schema: "omi",
+                table: "sample");
+        }
+    }
+}
