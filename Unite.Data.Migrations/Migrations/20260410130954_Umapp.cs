09c848e4382197988e978942da43db9a3d028c8a
vulasau <valiantsin.ulasau@gmail.com>
2026-04-10 15:11:51 +0200

    New migration of UMAPP.

---
+++ Unite.Data.Migrations/Migrations/20260410130954_Umapp.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Unite.Data.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class Umapp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                schema: "com",
+                table: "analysis_task_type",
+                columns: new[] { "id", "name", "value" },
+                values: new object[] { 8, "umapp", "umapp" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "com",
+                table: "analysis_task_type",
+                keyColumn: "id",
+                keyValue: 8);
+        }
+    }
+}
