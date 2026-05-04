2991b3b0664cf9f8aef5712074ac13fbf8011382
SemionV <semion.varennik@gmail.com>
2026-05-04 17:59:16 +0200

    CNV Profile Analysis Task Type

---
+++ Unite.Data.Migrations/Migrations/20260504150851_CnvProfileAnalysisTaskType.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Unite.Data.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class CnvProfileAnalysisTaskType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                schema: "com",
+                table: "analysis_task_type",
+                columns: new[] { "id", "name", "value" },
+                values: new object[] { 9, "cnv_profile", "cnv_profile" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "com",
+                table: "analysis_task_type",
+                keyColumn: "id",
+                keyValue: 9);
+        }
+    }
+}
