146ca5b338c3edb61accfdd67dddbc1a3f0266a0
vulasau <valiantsin.ulasau@gmail.com>
2026-06-18 14:40:47 +0200

    CED analysis migration.

---
+++ Unite.Data.Migrations/Migrations/20260522093828_CED.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Unite.Data.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class CED : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                schema: "com",
+                table: "analysis_task_type",
+                columns: new[] { "id", "name", "value" },
+                values: new object[,]
+                {
+                    { 10, "cedp", "cedp" },
+                    { 11, "cedg", "cedg" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "com",
+                table: "analysis_task_type",
+                keyColumn: "id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                schema: "com",
+                table: "analysis_task_type",
+                keyColumn: "id",
+                keyValue: 11);
+        }
+    }
+}
